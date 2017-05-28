f9dc5107f7f0e16c16249d03da1fb3c049dfb293
Just Burrow <just.burrow@lul.kr>
2017-05-28 14:50:29 +0900

    (ADD) GET 리퀘스트 출력 코드.

--- src/main/java/kr/lul/web/request/reflector/controller/ReflectController.java
+++ src/main/java/kr/lul/web/request/reflector/controller/ReflectController.java
@@ -2,6 +2,7 @@ package kr.lul.web.request.reflector.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,19 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RequestMapping("/**")
 public interface ReflectController {
+  /**
+   * @param req
+   * @param model
+   * @return
+   */
   @GetMapping
-  String get(HttpServletRequest request, Model model);
+  String get(HttpServletRequest req, Model model);
+
+  /**
+   * @param req
+   * @param model
+   * @return
+   */
+  @PostMapping
+  String post(HttpServletRequest req, Model model);
 }
--- src/main/java/kr/lul/web/request/reflector/controller/ReflectControllerImpl.java
+++ src/main/java/kr/lul/web/request/reflector/controller/ReflectControllerImpl.java
@@ -8,6 +8,10 @@ import org.springframework.ui.Model;
 import javax.servlet.http.HttpServletRequest;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.Collections.list;
 
 /**
  * @author justburrow
@@ -22,23 +26,43 @@ import java.net.URL;
    * @return
    */
   private String reflect(final HttpServletRequest req, final Model model) throws MalformedURLException {
+    // URL
     URL url = new URL(req.getRequestURL().toString());
-
     model.addAttribute("method", req.getMethod());
     model.addAttribute("url", url);
+
+    // HEADER
+    Map<String, String> headers = new LinkedHashMap<>();
+    list(req.getHeaderNames()).forEach(name -> headers.put(name, req.getHeader(name)));
+    model.addAttribute("headers", headers);
+    model.addAttribute("params", req.getParameterMap());
 
     return "reflect/basic";
   }
 
   /**
-   * @param request
+   * @param req
    * @param model
    * @return
    */
   @Override
-  public String get(final HttpServletRequest request, final Model model) {
+  public String get(final HttpServletRequest req, final Model model) {
     try {
-      return reflect(request, model);
+      return reflect(req, model);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * @param req
+   * @param model
+   * @return
+   */
+  @Override
+  public String post(HttpServletRequest req, Model model) {
+    try {
+      return reflect(req, model);
     } catch (MalformedURLException e) {
       throw new RuntimeException(e);
     }
